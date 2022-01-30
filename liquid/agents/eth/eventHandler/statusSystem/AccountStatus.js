379f32da69291f772c886d388c74208fcecd4fca
haolifeng <haolifengwang@126.com>
2022-01-30 14:15:31 +0800

    update tokenaccount

--- liquid/agents/eth/eventHandler/statusSystem/AccountStatus.js
+++ liquid/agents/eth/eventHandler/statusSystem/AccountStatus.js
@@ -1,8 +1,23 @@
 const BaseStatus = require('./BaseStatus');
 
 class AccountStatus extends BaseStatus {
-    constructor(id, chainAccess, storage){
-        super(id, chainAccess,storage);
+    constructor(accountAddress, chainAccess, storage){
+        super(accountAddress.toLowerCase(), chainAccess,storage);
+        this.type="Account";
+
+        this.properties.address = accountAddress.toLowerCase();
+        this.properties.health = "0";
+
+        this.properties.net_asset_value = "0";
+        this.properties.tokens = [];
+        this.properties.total_borrow_value = "0";
+        this.properties.total_collateral_value = "0";
+        this.properties.timestamp = "0";
+        this.accountTokenInstance = [];
+        this.properties.comp_reward = "0";
+        this.properties.rewardAddress = "";
+        this.properties.rewardBalances = '0';
+
 
     }
 }
--- liquid/agents/eth/eventHandler/statusSystem/AccountToken.js
+++ liquid/agents/eth/eventHandler/statusSystem/AccountToken.js
@@ -1,8 +1,30 @@
 const BaseStatus = require('./BaseStatus');
 
 class AccountToken extends BaseStatus {
-    constructor(id, chainAccess, storage){
-        super(id, chainAccess,storage);
+    constructor(accountAddress,marketAddress, chainAccess, storage){
+        super(accountAddress.toLowerCase() + '-' + marketAddress.toLowerCase().slice(2), chainAccess,storage);
+        this.type='AccountToken';
+
+        this.properties.account_address = accountAddress.toLowerCase();
+        this.properties.token_address = marketAddress.toLowerCase();
+        this.properties.is_entered = false;
+
+        this.properties.account_total_borrow = "0";
+        this.properties.account_total_repay = '0';
+        this.properties.account_total_supply = '0';
+        this.properties.account_total_redeem = '0';
+        this.properties.account_total_liquidated = '0';
+        this.properties.account_total_liquidate = "";
+
+        this.properties.lifetime_borrow_interest_accrued = '0';
+        this.properties.lifetime_supply_interest_accrued = '0';
+
+        this.properties.suppply_balance = "0";
+        this.properties.borrow_balance_underlying = "0";
+        this.properties.supply_balance_underlying = "0";
+        this.properties.timepstamp = "0";
+        this.market = undefined;
+        this.account = undefined;
 
     }
 }
